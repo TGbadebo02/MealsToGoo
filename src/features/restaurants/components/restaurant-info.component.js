de6d126a1a7e8d41ce3db80b01c359a64e0d4d56
TGbadebo02 <tj.gbadebo02@gmail.com>
2025-04-22 22:36:15 +0100

    update.

--- src/features/restaurants/components/restaurant-info.component.js
+++ src/features/restaurants/components/restaurant-info.component.js
@@ -3,6 +3,15 @@ import styled from "styled-components/native"
 import { Text, StyleSheet } from "react-native";
 import { Card } from "react-native-paper";
 
+const RestaurantCard = Styles(Card)`
+background-color: white;
+`;
+
+const RestaurantCardCover = styled(Card.Cover)`
+padding: 20px;
+background-Color: white;
+`;
+
 const Title = styled.Text`
   padding: 16px;
   color: red;
