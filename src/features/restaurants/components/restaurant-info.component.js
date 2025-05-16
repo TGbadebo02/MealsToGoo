fe792ab1990e1ab23f7c00440725a50c0a0a10a3
TGbadebo02 <tj.gbadebo02@gmail.com>
2025-05-16 15:04:38 +0100

    rating created

--- src/features/restaurants/components/restaurant-info.component.js
+++ src/features/restaurants/components/restaurant-info.component.js
@@ -1,7 +1,9 @@
 import React from "react";
+import star from "../../../../assets/star";
 import styled from "styled-components/native"
 import { Text, StyleSheet } from "react-native";
 import { Card } from "react-native-paper";
+import { SvgXml } from "react-native-svg";
 
 
 
@@ -29,6 +31,12 @@ const Info = styled.View`
 padding: ${(props) => props.theme.space[3]};
 `;
 
+const Rating = styled.View `
+flex-direction: row;
+padding-top: ${(props) => props.theme.space[2]};
+padding-bottom: ${(props) => props.theme.space[2]};
+`;
+
 export const RestaurantInfo = ({ restaurant = {} }) => {
     const {
         name = "Some Restaurant",
@@ -41,12 +49,18 @@ export const RestaurantInfo = ({ restaurant = {} }) => {
         rating = 4,
         isClosedTemporarily,
       } = restaurant;
-    
+      
+      const ratingArray = Array.from(new Array(Math.ceil(rating)));
       return (
         <RestaurantCard elevation={5}>
           <RestaurantCardCover key={name} style={styles.cover} source={{ uri: photos[0] }} />
           <Info>
           <Title>{name}</Title>
+          <Rating>
+          {ratingArray.map(() => (
+          <SvgXml xml={star} width={20} height={20}/>
+          ))}
+          </Rating>
           <Address>{address}</Address> 
           </Info>
         </RestaurantCard>
